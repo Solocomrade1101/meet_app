d68478538088f64dedf7bc2b695f5d0faceda968
e_kurbatov <e.kurbatov@smartshell.gg>
2025-05-29 11:38:54 +0300

    add fullscreen (remove)

--- src/App.tsx
+++ src/App.tsx
@@ -14,24 +14,24 @@ const App = () => {
 
     }, []);
 
-    useEffect(() => {
-        async function initTg() {
-            if (await isTMA()) {
-                init();
-
-                if (viewport.mount.isAvailable()) {
-                    await viewport.mount();
-                    viewport.expand();
-                }
-
-                if (viewport.requestFullscreen.isAvailable()) {
-                    await viewport.requestFullscreen();
-                }
-            }
-        }
-        initTg();
-
-    }, []);
+    // useEffect(() => {
+    //     async function initTg() {
+    //         if (await isTMA()) {
+    //             init();
+    //
+    //             if (viewport.mount.isAvailable()) {
+    //                 await viewport.mount();
+    //                 viewport.expand();
+    //             }
+    //
+    //             if (viewport.requestFullscreen.isAvailable()) {
+    //                 await viewport.requestFullscreen();
+    //             }
+    //         }
+    //     }
+    //     initTg();
+    //
+    // }, []);
     return (
         <>
             <Routes>
